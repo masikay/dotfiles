65cc412e1372f4c57791030db6a0c966e320ca81
Paolo Astolfi <paolo.astolfi@gmail.com>
2023-02-04 14:27:52 +0100

    Added packages for Ubuntu 22.04

--- linux/ubuntu/packages/setup.sh
+++ linux/ubuntu/packages/setup.sh
@@ -10,11 +10,20 @@ COMMENT=\#*
 sudo -v
 
 info "Installing Debian/Ubuntu packages ..."
-substep_info "Enabling GitHub CLI ppa ..."
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
+substep_info "Adding official Docker repository ..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update && sudo apt -y upgrade
 sudo apt -y install $(cat pkglist)
+
+substep_info "Installing exa ..."
+EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
+sudo unzip -q exa.zip bin/exa -d /usr/local
+rm -rf exa.zip
+
+substep_info "Adding user $(USER) to docker group"
+sudo usermod -aG docker $USER
 
 success "Finished installing Debian/Ubuntu packages."
 
