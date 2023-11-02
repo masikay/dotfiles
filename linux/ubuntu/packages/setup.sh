d09248e7bf958439adc977fe56625b46210ba59f
Paolo Astolfi <paolo.astolfi@gmail.com>
2023-11-02 08:34:04 +0100

    Fixed download of lsd for Ubuntu

--- linux/ubuntu/packages/setup.sh
+++ linux/ubuntu/packages/setup.sh
@@ -18,7 +18,7 @@ sudo apt -y install $(cat pkglist)
 
 substep_info "Installing lsd ..."
 LSD_VERSION=$(curl -s "https://api.github.com/repos/lsd-rs/lsd/releases/latest" | grep -Po '"tag_name": "\vK[0-9.]+')
-curl -Lo lsd.deb "https://github.com/olsd-rs/lsd/releases/latest/download/lsd_${LSD_VERSION}_amd64.deb"
+curl -Lo lsd.deb "https://github.com/lsd-rs/lsd/releases/latest/download/lsd_${LSD_VERSION}_amd64.deb"
 sudo dpkg -i lsd.deb
 rm -rf lsd.deb
 
